22c89425d89f77243510fe5af44e09f08134fc9b
john-sonz <janzborowski99@gmail.com>
2017-11-19 18:50:11 +0100

    Added road tracking system

--- js/kulki.js
+++ js/kulki.js
@@ -2,7 +2,7 @@ const BOARD_SIZE = 9;
 const START_BALLS_AMOUNT = 3;
 const COLORS = ['#b7724a', '#b7724a', '#27cfac', '#4031fc', '#dc87f8', '#a45b67', '#bdd654'];
 let boardTab = [];
-//let idsTab = [];
+let idsTab = [];
 let table = document.getElementById('table');
 let clicked = false;
 let anyGood;
@@ -21,13 +21,13 @@ function createTable() {
     table.id = 'table';
     for (let i = 0; i < BOARD_SIZE; i++) {
         let boardRow = [];
-        //let idsRow = [];
+        let idsRow = [];
         for (let j = 0; j < BOARD_SIZE; j++) {
             boardRow.push(0);
-            //idsRow.push([]);
+            idsRow.push([]);
         }
         boardTab.push(boardRow);
-        //idsTab.push(idsRow);
+        idsTab.push(idsRow);
         let tr = document.createElement('tr');
         for (let j = 0; j < BOARD_SIZE; j++) {
             let td = document.createElement('td');
@@ -55,6 +55,17 @@ function createTable2() {
         table.appendChild(tr);
     }
     document.body.appendChild(table);
+    table = document.createElement('table');
+    for (let i = 0; i < BOARD_SIZE; i++) {
+        let tr = document.createElement('tr');
+        for (let j = 0; j < BOARD_SIZE; j++) {
+            let td = document.createElement('td');
+            td.id = 'a' + '_' + i + '_' + j;
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+    document.body.appendChild(table);
 }
 
 function updateTable2() {
@@ -63,24 +74,38 @@ function updateTable2() {
             let td = document.getElementById('h' + '_' + i + '_' + j);
             td.innerHTML = boardTab[i][j];
             let td2 = document.getElementById('a' + '_' + i + '_' + j);
-            //td2.innerHTML = idsTab[i][j];
+            td2.innerHTML = idsTab[i][j];
         }
     }
 }
 
 function ballClickHandler() {
+    let tds = document.getElementsByClassName('free');
+    resetTabs();
+    clearRoad();
     if (this.className.includes('clicked')) {
         this.classList.remove('clicked');
         clicked = false;
+        for (let i = 0; i < tds.length; i++) {
+            tds[i].removeEventListener('mouseover', previewRoad);
+        }
     } else {
         if (clicked) {
             document.querySelector('.clicked').classList.remove('clicked');
         } else {
             clicked = true;
+            for (let i = 0; i < tds.length; i++) {
+                tds[i].addEventListener('mouseover', previewRoad);
+            }
 
         }
-        this.classList.add('clicked');
+        this.classList.add('clicked');        
     }
+    let id = this.parentNode.id.split('_');
+    console.log(id);
+    let tdY = parseInt(id[0]);
+    let tdX = parseInt(id[1]);
+    firstStep(tdY, tdX);
     updateTable2();
     console.log(clicked);
 
@@ -89,7 +114,7 @@ function ballClickHandler() {
 function resetTabs() {
     for (let i = 0; i < BOARD_SIZE; i++) {
         for (let j = 0; j < BOARD_SIZE; j++) {
-            //idsTab[i][j] = [];
+            idsTab[i][j] = [];
             if (boardTab[i][j] != 'X') boardTab[i][j] = 0;
         }
     }
@@ -134,7 +159,8 @@ function moveBall() {
         startY = parseInt(chosenBallId[0]);
         startX = parseInt(chosenBallId[1]);
         firstStep(startY, startX);
-        //console.log(idsTab[tdY][tdY]);
+
+        console.log(idsTab[tdY][tdX]);
         if (boardTab[tdY][tdY] != 0) {
             clicked = false;
             chosenBall.id = 'b' + '_' + tdY + '_' + tdX;
@@ -146,6 +172,10 @@ function moveBall() {
             chosenBall.parentElement.classList.add('free');
             chosenBall.parentNode.removeChild(chosenBall);
             boardTab[tdY][tdX] = 'X';
+            let tds = document.getElementsByClassName('free');
+            for (let i = 0; i < tds.length; i++) {
+                tds[i].removeEventListener('mouseover', previewRoad);
+            }
             boardTab[startY][startX] = 0;
             this.removeEventListener('click', moveBall);
             this.classList.remove('free');
@@ -155,6 +185,11 @@ function moveBall() {
             });
             ball.y = tdY;
             ball.x = tdX;
+            idsTab[tdY][tdX].forEach((el) => {
+                document.getElementById(el).style.backgroundColor = 'gray';
+            });
+            this.style.backgroundColor = 'gray';
+            setTimeout(clearRoad, 500);
         }
         resetTabs();
         console.log(clicked);
@@ -162,34 +197,34 @@ function moveBall() {
 }
 
 function checkRoad(y, x, start) {
-    /*let preTab = idsTab[y][x].slice();
-    preTab.push(y + '_' + x);*/
+    let preTab = idsTab[y][x].slice();
+    preTab.push(y + '_' + x);
     if (x - 1 >= 0) {
         if (boardTab[y][x - 1] == 0) {
             boardTab[y][x - 1] = start + 1;
             anyGood = true;
-            //idsTab[y][x - 1] = preTab;
+            idsTab[y][x - 1] = preTab;
         }
     }
     if (x + 1 < BOARD_SIZE) {
         if (boardTab[y][x + 1] == 0) {
             boardTab[y][x + 1] = start + 1;
             anyGood = true;
-            //idsTab[y][x + 1] = preTab;
+            idsTab[y][x + 1] = preTab;
         }
     }
     if (y - 1 >= 0) {
         if (boardTab[y - 1][x] == 0) {
             boardTab[y - 1][x] = start + 1;
             anyGood = true;
-            //idsTab[y - 1][x] = preTab;
+            idsTab[y - 1][x] = preTab;
         }
     }
     if (y + 1 < BOARD_SIZE) {
         if (boardTab[y + 1][x] == 0) {
             boardTab[y + 1][x] = start + 1;
             anyGood = true;
-            //idsTab[y + 1][x] = preTab;
+            idsTab[y + 1][x] = preTab;
         }
     }
 
@@ -213,6 +248,24 @@ function nextStep(start) {
         nextStep(start + 1);
     }
     updateTable2();
+}
+
+function clearRoad() {
+    let tds = document.getElementsByTagName('td');
+    for (let i = 0; i < tds.length; i++) {
+        tds[i].style.backgroundColor = 'white';
+    }
+}
+
+function previewRoad() {
+    clearRoad();
+    let id = this.id.split('_');
+    let tdY = parseInt(id[0]);
+    let tdX = parseInt(id[1]);
+    idsTab[tdY][tdX].forEach((el) => {
+        document.getElementById(el).style.backgroundColor = 'pink';
+    });
+    this.style.backgroundColor = 'pink';
 }
 
 function init() {
