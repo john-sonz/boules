349dfd4e5118d3b724ea3ea5c4579c8577c5db04
john-sonz <janzborowski99@gmail.com>
2017-11-19 17:35:47 +0100

    Fixing and breaking stuff

--- js/kulki.js
+++ js/kulki.js
@@ -6,6 +6,7 @@ let idsTab = [];
 let table = document.getElementById('table');
 let clicked = false;
 let anyGood;
+let balls = [];
 
 class Ball {
     constructor(color, posY, posX) {
@@ -42,14 +43,65 @@ function createTable() {
 
 }
 
-function ballClickHandler(e) {
-    if (clicked) {
-        document.querySelector('.clicked').classList.remove('clicked');
-    } else {
-        clicked = true;
+function createTable2() {
+    let table = document.createElement('table');
+    for (let i = 0; i < BOARD_SIZE; i++) {
+        let tr = document.createElement('tr');
+        for (let j = 0; j < BOARD_SIZE; j++) {
+            let td = document.createElement('td');
+            td.id = 'h' + '_' + i + '_' + j;
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
     }
-    this.classList.add('clicked');    
+    document.body.appendChild(table);
+    table = document.createElement('table');
+    for (let i = 0; i < BOARD_SIZE; i++) {
+        let tr = document.createElement('tr');
+        for (let j = 0; j < BOARD_SIZE; j++) {
+            let td = document.createElement('td');
+            td.id = 'a' + '_' + i + '_' + j;
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
+    document.body.appendChild(table);
+}
 
+function updateTable2() {
+    for (let i = 0; i < BOARD_SIZE; i++) {
+        for (let j = 0; j < BOARD_SIZE; j++) {
+            let td = document.getElementById('h' + '_' + i + '_' + j);
+            td.innerHTML = boardTab[i][j];
+            let td2 = document.getElementById('a' + '_' + i + '_' + j);
+            td2.innerHTML = idsTab[i][j];
+        }
+    }
+}
+
+function ballClickHandler() {
+    if (this.className.includes('clicked')) {
+        this.classList.remove('clicked');
+        clicked = false;
+    } else {
+        if (clicked) {
+            document.querySelector('.clicked').classList.remove('clicked');
+        } else {
+            clicked = true;
+            this.classList.add('clicked');
+        }
+    }
+    updateTable2();
+
+}
+
+function resetTabs() {
+    for (let i = 0; i < BOARD_SIZE; i++) {
+        for (let j = 0; j < BOARD_SIZE; j++) {
+            idsTab[i][j] = [];
+            if (boardTab[i][j] != 'X') boardTab[i][j] = 0;
+        }
+    }
 }
 
 function drawBalls() {
@@ -65,7 +117,8 @@ function drawBalls() {
             drawnYs.push(y);
             let colorIndex = Math.floor(Math.random() * 7);
             let ball = new Ball(COLORS[colorIndex], y, x);
-            boardTab[y][x] = ball;
+            balls.push(ball);
+            boardTab[y][x] = 'X';
             let td = document.getElementById(y + '_' + x);
             let ballDiv = document.createElement('div');
             ballDiv.id = 'b_' + y + '_' + x;
@@ -81,17 +134,21 @@ function drawBalls() {
 
 function moveBall() {
     if (clicked) {
+        let id = this.id.split('_');
+        let tdY = parseInt(id[0]);
+        let tdX = parseInt(id[1]);
+        console.log(id);
+        console.log(tdY + ' ' + tdX);
         let chosenBall = document.querySelector('.clicked');
         let chosenBallId = chosenBall.id.split('_');
-        chosenBallId.shift();        
-        let startPos = {
-            y: parseInt(chosenBallId[0]),
-            x: parseInt(chosenBallId[1])
-        };
-        console.log(startPos);
-        firstStep(startPos.y, startPos.x);
-        let id = this.id.split('_');
-        if (idsTab[id[0]][id[1]].length > 0) {
+        chosenBallId.shift();
+        startY = parseInt(chosenBallId[0]);
+        startX = parseInt(chosenBallId[1]);
+        firstStep(startY, startX);
+        console.log(idsTab[tdY][tdY]);
+        if (boardTab[tdY][tdY] != 0) {
+            clicked = false;            
+            chosenBall.id = 'b' + '_' + tdY + '_' + tdX;            
             chosenBall.classList.remove('clicked');
             let clonedBall = chosenBall.cloneNode();
             clonedBall.addEventListener('click', ballClickHandler);
@@ -99,13 +156,18 @@ function moveBall() {
             chosenBall.parentElement.addEventListener('click', moveBall);
             chosenBall.parentElement.classList.add('free');
             chosenBall.parentNode.removeChild(chosenBall);
-            boardTab[id[0]][id[1]] = boardTab[startPos.y][startPos.x];
-            boardTab[startPos.y][startPos.x] = 0;
+            boardTab[tdY][tdX] = 'X';
+            boardTab[startY][startX]= 0;
             this.removeEventListener('click', moveBall);
             this.classList.remove('free');
-            clicked = false;
-
-        }
+            let ball = balls.find((el) => {
+                if (el.x == startX && el.y == startY) return true;
+                else return false;
+            });
+            ball.y = tdY;
+            ball.x = tdX;
+        }        
+        resetTabs();
     }
 }
 
@@ -113,54 +175,37 @@ function checkRoad(y, x, start) {
     let preTab = idsTab[y][x].slice();
     preTab.push(y + '_' + x);
     if (x - 1 >= 0) {
-        if (boardTab[y][x - 1] == 0 || boardTab[y][x - 1] == 'M') {
+        if (boardTab[y][x - 1] == 0) {
             boardTab[y][x - 1] = start + 1;
-            if (boardTab[y][x - 1] == 'M') {
-                anyGood = false;
-                return;
-            }
             anyGood = true;
             idsTab[y][x - 1] = preTab;
         }
     }
     if (x + 1 < BOARD_SIZE) {
-        if (boardTab[y][x + 1] == 0 || boardTab[y][x + 1] == 'M') {
+        if (boardTab[y][x + 1] == 0) {
             boardTab[y][x + 1] = start + 1;
-            if (boardTab[y][x - 1] == 'M') {
-                anyGood = false;
-                return;
-            }
             anyGood = true;
             idsTab[y][x + 1] = preTab;
         }
     }
     if (y - 1 >= 0) {
-        if (boardTab[y - 1][x] == 0 || boardTab[y - 1][x] == 'M') {
+        if (boardTab[y - 1][x] == 0) {
             boardTab[y - 1][x] = start + 1;
-            if (boardTab[y - 1][x] == 'M') {
-                anyGood = false;
-                return;
-            }
             anyGood = true;
             idsTab[y - 1][x] = preTab;
         }
     }
     if (y + 1 < BOARD_SIZE) {
-        if (boardTab[y + 1][x] == 0 || boardTab[y + 1][x] == 'M') {
+        if (boardTab[y + 1][x] == 0) {
             boardTab[y + 1][x] = start + 1;
-            if (boardTab[y + 1][x] == 'M') {
-                anyGood = false;
-                return;
-            }
             anyGood = true;
             idsTab[y + 1][x] = preTab;
         }
     }
+
 }
 
 function firstStep(y, x) {
-    console.log(y);
-    console.log(x);
     checkRoad(y, x, 0);
     nextStep(1);
 }
@@ -176,11 +221,14 @@ function nextStep(start) {
     }
     if (anyGood) {
         nextStep(start + 1);
-    }
+    }    
+    updateTable2();
 }
 
 function init() {
     createTable();
+    createTable2();
+    updateTable2();
 }
 
 document.addEventListener('DOMContentLoaded', init);
